c7e7c1b3f338d702260afbac3e96389ffaee58c0
HinJaban <nikita.bek22@gmail.com>
2025-01-06 17:55:47 +0300

    Класс  Карты

--- Class.h
+++ Class.h
@@ -1,5 +1,20 @@
 #ifndef CLASS_H
 #define CLASS_H
+enum Suit { clubs, diamonds, hearts,spades };
+class card
+{
+  private:
+    int number;              // достоинство карты
+    Suit suit;               // масть
+  public:
+    card ()                  // конструктор без аргументов
+      { }
+                   // конструктор с двумя аргументами
+    card (int n, Suit s) : number(n), suit(s)
+      { }
+    void display();          // вывод карты на экран
+    bool isEqual(card);      // результат сравнения карт
+};
 
 
 #endif
--- main.cpp
+++ main.cpp
@@ -2,52 +2,47 @@
 #include "Class.h"
 #include "Class.cpp"
 
-const int clubs = 0;        // масти
-const int diamonds = 1;
-const int hearts = 2; 
-const int spades = 3;
-const int jack = 11;        // достоинства карт
-const int queen = 12;
-const int king = 13;
-const int ace = 14;
-////////////////////////////////////////////////
-struct card
-{
-int number;             // доистоинство
-int suit;               // масть
-};
 
+
+
+//////////////////////////////////////////////////////////
 int main()
 {
-    setlocale(LC_ALL, "ru_RU.UTF-8");
-   card temp, chosen, prize;  // три карты
-int position;
-card card1 = { 7, clubs };     // инициализация карты 1
-std::cout << "Карта 1: 7 треф\n";
-card card2 = { jack, hearts }; // инициализация карты 2
-std::cout << "Карта 2: валет червей\n";
-card card3 = { ace, spades };  // инициализация карты 3
-std::cout << "Карта 3: туз пик\n ";
-prize = card3;                 // запоминание карты 3
-std::cout << "Меняем местами карту 1 и карту 3...\n";
-temp = card3; card3 = card1; card1 = temp;
-std::cout << "Меняем местами карту 2 и карту 3...\n";
-temp = card3; card3 = card2; card2 = temp;
-std::cout << "Меняем местами карту 1 и карту 2...\n";
-temp = card2; card2 = card1; card1 = temp;
-std::cout << "На какой позиции (1,2 или 3) теперь туз пик? ";
-std::cin>>position;
-switch(position) 
-{
-case 1: chosen = card1; break;
-case 2: chosen = card2; break;
-case 3: chosen = card3; break;
+    setlocale(LC_ALL, "ru-RU.UTF-8");
+  card temp, chosen, prize;  // 3 карты
+  int position;
+  card card1(7, clubs);      // определение и инициализация card1
+  std::cout << "\nКарта 1: ";
+  card1.display();           // вывод card1
+  card card2(jack, hearts);  // определение и инициализация card2
+  std::cout <<  "\nКарта 2: ";
+  card2.display();           // вывод card2
+  card card3(ace, spades);   // определение и инициализация card3
+  std::cout <<  "\nКарта 3: ";
+  card3.display();           // вывод card3
+  prize = card3;             // карту prize будет необходимо угадать
+  std::cout << "\nМеняем местами карты 1 и 3...";
+  temp = card3; card3 = card1; card1 = temp;
+  std::cout << "\nМеняем местами карты 2 и 3...";
+  temp = card3; card3 = card2; card2 = temp;
+  std::cout << "\nМеняем местами карты 1 и 2...";
+  temp = card2; card2 = card1; card1 = temp;
+  std::cout << "\nНа какой позиции (1,2 или 3) теперь ";
+  prize.display();           // угадываемая карта
+  std::cout << "?";
+  std::cin >> position;           // ввод позиции игроком
+  switch (position)
+  {                          // chosen – карта на позиции,
+    case 1:chosen = card1; break; // выбранной игроком
+    case 2:chosen = card2; break;
+    case 3:chosen = card3; break;
+  }
+  if(chosen.isEqual(prize))       // сравнение карт
+    std::cout << "Правильно! Вы выиграли!";
+  else
+    std::cout << "Неверно. Вы проиграли.";
+   std::cout << " Вы выбрали карту ";
+  chosen.display();               // вывод выбранной карты
+  std::cout << std::endl;
+  return 0;
 }
-if(chosen.number == prize.number && // сравнение карт
-   chosen.suit == prize.suit)
-   std::cout << "Правильно! Вы выиграли!\n";
-   else
-   std::cout << "Неверно. Вы проиграли.\n";
-   return 0;
-}
-// Вызов конструктора
