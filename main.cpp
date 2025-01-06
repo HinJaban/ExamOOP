ddded87a7310caa9741e91c9ef42e53123518970
HinJaban <nikita.bek22@gmail.com>
2025-01-06 16:03:32 +0300

    6  класс

--- main.cpp
+++ main.cpp
@@ -6,15 +6,14 @@
 int main()
 {
     setlocale(LC_ALL, "ru_RU.UTF-8");
-    Distance dist1, dist3;            // две длины
-    Distance dist2(11,6.25);   // определение и инициализация
-    dist1.getdist();             // ввод значений для dist2
-    dist3.add_dist(dist1, dist2); // dist3 = dist1 + dist2
-    /* вывод всех длин */
-    std::cout << "\ndist1 = "; dist1.showdist();
-    std::cout << "\ndist2 = "; dist2.showdist();
-    std::cout << "\ndist3 = "; dist3.showdist();
-    std::cout << std::endl;
-    return 0;
+    Distance dist1(11,6.25);   // определение и инициализация
+Distance dist2(dist1);     //два конструктора
+Distance dist3 = dist1;    //с одним аргументом
+/* вывод всех длин */
+std::cout << "\ndist1 = "; dist1.showdist();
+std::cout << "\ndist2 = "; dist2.showdist();
+std::cout << "\ndist3 = "; dist3.showdist();
+std::cout << std::endl;
+return 0;
 }
 // Вызов конструктора
