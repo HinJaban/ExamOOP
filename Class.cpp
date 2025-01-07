71bb54e1bdb0042b228e4ddd4c04deb414d25335
HinJaban <nikita.bek22@gmail.com>
2025-01-07 09:16:45 +0300

    Шаблон1

--- Class.cpp
+++ Class.cpp
@@ -2,35 +2,3 @@
 #include "Class.h"
 
 
-const int jack = 11;         // именованные достоинства карт
-const int queen = 12; 
-const int king = 13;
-const int ace = 14;
-//--------------------------------------------------------
-void card::display()         // вывод карты на экран
-{
-    setlocale(LC_ALL, "ru-RU.UTF-8");
-  if( number >= 2 && number <= 10 )
-    std::cout << number ;
-  else
-    switch(number)
-    {
-      case jack:   std::cout << "Валет ";  break;
-      case queen:  std::cout << "Дама ";   break;
-      case king:   std::cout << "Король "; break;
-      case ace:    std::cout << "Туз ";    break;
-    }
-  switch(suit)
-  {
-    case clubs:     std::cout << "треф"   ;break;
-    case diamonds:  std::cout << "бубен"  ;break;
-    case hearts:    std::cout << "червей" ;break;
-    case spades:    std::cout << "пик"    ;break;
-  }
-}
-//--------------------------------------------------------
-bool card::isEqual(card c2)  // сравнение двух карт
-{
-  return (number==c2.number && suit==c2.suit)? true : false;
-}
-//////////////////////////////////////////////////////////
--- main.cpp
+++ main.cpp
@@ -1,48 +1,27 @@
 #include <iostream>
 #include "Class.h"
 #include "Class.cpp"
-
-
-
-
-//////////////////////////////////////////////////////////
+//Шаблон функции вычисления модуля числа 
+#include <iostream>
+template <class T>         // шаблон функции
+T abs(T n)
+{
+return (n<0) ? -n: n;
+}
+//-------------------------------------------------------------------
 int main()
 {
-    setlocale(LC_ALL, "ru-RU.UTF-8");
-  card temp, chosen, prize;  // 3 карты
-  int position;
-  card card1(7, clubs);      // определение и инициализация card1
-  std::cout << "\nКарта 1: ";
-  card1.display();           // вывод card1
-  card card2(jack, hearts);  // определение и инициализация card2
-  std::cout <<  "\nКарта 2: ";
-  card2.display();           // вывод card2
-  card card3(ace, spades);   // определение и инициализация card3
-  std::cout <<  "\nКарта 3: ";
-  card3.display();           // вывод card3
-  prize = card3;             // карту prize будет необходимо угадать
-  std::cout << "\nМеняем местами карты 1 и 3...";
-  temp = card3; card3 = card1; card1 = temp;
-  std::cout << "\nМеняем местами карты 2 и 3...";
-  temp = card3; card3 = card2; card2 = temp;
-  std::cout << "\nМеняем местами карты 1 и 2...";
-  temp = card2; card2 = card1; card1 = temp;
-  std::cout << "\nНа какой позиции (1,2 или 3) теперь ";
-  prize.display();           // угадываемая карта
-  std::cout << "?";
-  std::cin >> position;           // ввод позиции игроком
-  switch (position)
-  {                          // chosen – карта на позиции,
-    case 1:chosen = card1; break; // выбранной игроком
-    case 2:chosen = card2; break;
-    case 3:chosen = card3; break;
-  }
-  if(chosen.isEqual(prize))       // сравнение карт
-    std::cout << "Правильно! Вы выиграли!";
-  else
-    std::cout << "Неверно. Вы проиграли.";
-   std::cout << " Вы выбрали карту ";
-  chosen.display();               // вывод выбранной карты
-  std::cout << std::endl;
-  return 0;
+int int1 = 5;
+int int2 = -6;
+long lon1 = 70000L;
+long lon2 = -80000L;
+double dub1 = 9.95;
+double dub2 = -10.15;
+// осуществления вызовов
+std::cout << "\nabs(" << int1 << ")=" << abs(int1); //abs(int)
+std::cout << "\nabs(" << int2 << ")=" << abs(int2); //abs(int)
+std::cout << "\nabs(" << lon1 << ")=" << abs(lon1); //abs(long)
+std::cout << "\nabs(" << lon2 << ")=" << abs(lon2); //abs(long)
+std::cout << "\nabs(" << dub1 << ")=" << abs(dub1); //abs(double)
+std::cout << "\nabs(" << dub2 << ")=" << abs(dub2); //abs(double)
 }
