eb23e7e9fdf604d1a41c790486b11680ef1e3308
HinJaban <nikita.bek22@gmail.com>
2025-01-08 15:24:08 +0300

    УчебеыйПример

--- main.cpp
+++ main.cpp
@@ -2,25 +2,15 @@
 #include "Class.h"
 #include "Class.cpp"
 #include <string>
-
-
-int main() 
+template <class T>
+T func(T number)
 {
-setlocale(LC_ALL, "ru_RU.UTF-8");
-const unsigned long SIZE = 10000; // объем памяти
-char* ptr;                        // указатель на адрес в памяти
-
-try
-{
-ptr = new char[SIZE];             // разместить в памяти SIZE байт
-}
-catch(std::bad_alloc)                  // обработчик исключений
-{
-std::cout << "\n Исключение bad_alloc: невозможно разместить данные в памяти.\n";
-return(1);
-}
-delete[] ptr;                     // освободить память
-std::cout << "\nПамять используется без сбоев.\n";
-return 0;
+  return 2 * number;
 }
 
+int main()
+{
+  setlocale(LC_ALL, "ru_RU.UTF-8");
+
+  std::cout << func(1000 000 000 000 0L) << std::endl;
+}
