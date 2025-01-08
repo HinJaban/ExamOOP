988282b7639f644d984d017f88a9063a7a6b3396
HinJaban <nikita.bek22@gmail.com>
2025-01-08 18:53:04 +0300

    УчебеыйПример3

--- main.cpp
+++ main.cpp
@@ -2,71 +2,65 @@
 #include "Class.h"
 #include "Class.cpp"
 #include <string>
-class Car
+class Divide 
 {
-
 private:
-  int velocity;
-
+int a, b, c;
 public:
-  class InchesEx
-  {
-    public:
+class InchesEx              // класс исключений
+{
+public:
+std::string origin;              // для имени функции
+float iValue;               // для хранения ошибочного
+                            // значения
+InchesEx(std::string or, float in) // конструктор с
+                            // двумя аргументами
+{
+origin = or;                // сохраненная строка
+                            // с именем виновника ошибки
+iValue = in;                // сохраненное неправильное 
+                            // значение дюймов
+}
+                            // конец класса исключений
+};
+Divide(): a(0), b(1)
+{ 
+  c=a/b;
+}
+Divide(int A, int B): a(A), b(B) 
+{ 
+  if(B==0)
+    throw InchesEx("Конструктор с двумя аргументами", B);
+  a=A;
+  b=B;
+  c=a/b;
+}
+void setValue(int A, int B)
+{
+  if(B==0)
+    throw InchesEx("Сеттер", B);
+  a=A;
+  b=B;
+}
+int getAnswer(){
+  return c;
+}
 
-    std::string origin;
-    int iValue;
-    InchesEx(std::string or, int in)
-    {
-      origin = or;
-      iValue = in;
-    }
-  };
-
-  // конструктор по умолчанию
-  Car() : velocity(0) {};
-  // конструктор с одним параметром
-  Car(int v)
-  {
-    if (v > 120)
-    {
-      throw InchesEx("Конструктор с одним аргументом", v); // Если скорость больше 120
-    velocity = 120;
-    }
-    else velocity = v;
-  };
-  int getVelocity()
-  {
-    return velocity;
-  }
-  void setVelocity(int v)
-  {
-    // условие исключения
-    if (v > 120)
-    {
-      throw InchesEx("функция setVelocity()", v); // генерируется исключения
-    velocity = 120;
-    }
-    else velocity = v;
-  }
 };
 
 int main()
 {
-
   setlocale(LC_ALL, "ru_RU.UTF-8");
-  Car Maz;
-  int v = 130;
-  try
+  try 
   {
-    std::cout << "Скорость Car : " << Maz.getVelocity() << std::endl;
-    Maz.setVelocity(v);
-    std::cout << "Скорость Car : " << Maz.getVelocity() << std::endl;
-    }
-
-  catch (Car::InchesEx ix )
-  {
-    std::cout << "\nОшибка инициализации. Виновник: " << ix.origin << ".\n Введенное значение скорости " << ix.iValue<< " слишком большое. ";
+    Divide A;
+    A.setValue(5,0);
   }
-  Maz.setVelocity(v);
-  std::cout << "Скорость Car : " << Maz.getVelocity() << std::endl;
+  catch(Divide::InchesEx ix)
+  {
+std::cout << "\nОшибка инициализации. Виновник: " << ix.origin
+<< ".\n Введенное значение делимого " << ix.iValue
+<< " равно ноль. ";
+  }
+return 0;
 }
