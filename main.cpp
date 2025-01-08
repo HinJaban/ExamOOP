a1fcd44ca0ab96e3a00a28451bef53391d289282
HinJaban <nikita.bek22@gmail.com>
2025-01-08 16:58:32 +0300

    УчебеыйПример2

--- main.cpp
+++ main.cpp
@@ -2,15 +2,71 @@
 #include "Class.h"
 #include "Class.cpp"
 #include <string>
-template <class T>
-T func(T number)
+class Car
 {
-  return 2 * number;
-}
+
+private:
+  int velocity;
+
+public:
+  class InchesEx
+  {
+    public:
+
+    std::string origin;
+    int iValue;
+    InchesEx(std::string or, int in)
+    {
+      origin = or;
+      iValue = in;
+    }
+  };
+
+  // конструктор по умолчанию
+  Car() : velocity(0) {};
+  // конструктор с одним параметром
+  Car(int v)
+  {
+    if (v > 120)
+    {
+      throw InchesEx("Конструктор с одним аргументом", v); // Если скорость больше 120
+    velocity = 120;
+    }
+    else velocity = v;
+  };
+  int getVelocity()
+  {
+    return velocity;
+  }
+  void setVelocity(int v)
+  {
+    // условие исключения
+    if (v > 120)
+    {
+      throw InchesEx("функция setVelocity()", v); // генерируется исключения
+    velocity = 120;
+    }
+    else velocity = v;
+  }
+};
 
 int main()
 {
-  setlocale(LC_ALL, "ru_RU.UTF-8");
 
-  std::cout << func(1000 000 000 000 0L) << std::endl;
+  setlocale(LC_ALL, "ru_RU.UTF-8");
+  Car Maz;
+  int v = 130;
+  try
+  {
+    std::cout << "Скорость Car : " << Maz.getVelocity() << std::endl;
+    Maz.setVelocity(v);
+    std::cout << "Скорость Car : " << Maz.getVelocity() << std::endl;
+    }
+
+  catch (Car::InchesEx ix )
+  {
+    std::cout << "\nОшибка инициализации. Виновник: " << ix.origin << ".\n Введенное значение скорости " << ix.iValue<< " слишком большое. ";
+  }
+  Maz.setVelocity(v);
+  std::cout << "Скорость Car : " << Maz.getVelocity() << std::endl;
 }
