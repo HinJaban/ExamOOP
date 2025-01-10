0a9dd75103bc0e42deaa2c8c341560dbf283d935
HinJaban <nikita.bek22@gmail.com>
2025-01-10 17:33:34 +0300

    Индексация по ссылке

--- main.cpp
+++ main.cpp
@@ -1,56 +1,40 @@
 // демонстрация создания безопасного массива, проверяющего
 // свои индексы при использовании
-// используются отдельные функции для установки и получения значения
+// используется общая функция для установки и получения значения
 #include <iostream>
-#include <process.h>   // для функции exit
-const int LIMIT = 100; // размер массива
-///////////////////////////////////////////////////
+#include <process.h>                 // для функции exit
+const int LIMIT = 100;               // размер массива
+///////////////////////////////////////////////////////
 class safearray
 {
-    
 private:
-    int arr[LIMIT];
-
+int arr [ LIMIT ];
 public:
-    // установка значения элемента массива
-    void putel(int n, int elvalue)
-    {
-        setlocale(LC_ALL, "ru-RU.UTF-8");
-        if (n < 0 || n >= LIMIT)
-        {
-            std::cout << "\nОшибочный индекс!";
-            exit(1);
-        }
-        arr[n] = elvalue;
-    }
-    // получение значения элемента массива
-    int getel(int n) const
-    {
-        setlocale(LC_ALL, "ru-RU.UTF-8");
-        if (n < 0 || n >= LIMIT)
-        {
-            std::cout << "\nОшибочный индекс!";
-            exit(1);
-        }
-        return arr[n];
-    }
-};
-//////////////////////////////////////////////////////
-int main()
+// стоит обратить внимание, что функция возвращает ссылку!
+int& access ( int n )
 {
-    safearray sa1;
     setlocale(LC_ALL, "ru-RU.UTF-8");
+if( n < 0 || n >= LIMIT )
+{ std::cout << "\nОшибочный индекс!"; exit( 1 ); }
+return arr [ n ];
+}
+};
+///////////////////////////////////////////////////////
+int main ( )
+{
+setlocale(LC_ALL, "ru-RU.UTF-8");
+safearray sa1;
 
-    // задаем значения элементов
-    for (int j = 0; j < LIMIT; j++)
-        sa1.putel(j, j * 10);
+// задаем значения элементов
+for ( int j = 0; j < LIMIT; j++ )
+sa1.access ( j ) = j * 10;       // используем функцию слева от знака =
 
-    // показываем элементы
-    for (int j = 0; j < LIMIT; j++)
-    {
-        int temp = sa1.getel(j);
-        std::cout << "Элемент " << j << " равен " << temp << std::endl;
-    }
+// показываем элементы
+for ( int j = 0; j < LIMIT; j++ )
+{
+int temp =  sa1.access ( j );    // используем функцию справа от знака =
+std::cout << "Элемент " << j << " равен " << temp << std::endl;
+}
 
-    return 0;
+return 0;
 }
